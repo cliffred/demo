188aad64f7fc327bc75bd23a3a777067c278b854
Cliffred van Velzen <cliffred@cliffred.nl>
2021-03-24 23:19:56 +0100

    Sample tests

---
+++ src/test/kotlin/red/cliff/demo/FooTest.kt
@@ -0,0 +1,22 @@
+package red.cliff.demo
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.fail
+
+class FooTest {
+
+    @Test
+    internal fun `just some test`() {
+
+    }
+
+    @Test
+    internal fun `just some other test`() {
+
+    }
+
+    @Test
+    internal fun `this is a failing test`() {
+        fail("sorry")
+    }
+}
